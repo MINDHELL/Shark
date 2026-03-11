b7af98702d9114ed4bbfc53a1625f62240f007ca
MINDHELL <Tgpurpose640@gmail.com>
2026-03-11 12:22:29 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -69,10 +69,10 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             caption=(
                 f"👋 {query.from_user.username}\n\n"
                 f"🎖️ Available Plans :\n\n"
-                f"● {PRICE1}  For 0 Days Prime Membership\n\n"
+                f"● {PRICE1}  For 7 Days Prime Membership\n\n"
                 f"● {PRICE2}  For 1 Month Prime Membership\n\n"
-                f"● {PRICE3}  For 3 Months Prime Membership\n\n"
-                f"● {PRICE4}  For 6 Months Prime Membership\n\n"
+                f"● {PRICE3}  For 2 Months Prime Membership\n\n"
+                f"● {PRICE4}  For 3 Months Prime Membership\n\n"
                 f"● {PRICE5}  For 1 Year Prime Membership\n\n\n"
                 f"💵 ASK UPI ID TO ADMIN AND PAY THERE -  <code>{UPI_ID}</code>\n\n\n"
                 f"♻️ After Payment You Will Get Instant Membership \n\n\n"
