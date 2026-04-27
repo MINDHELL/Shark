91d181a0ec67680511c14d9430f30e124aca3553
MINDHELL <Tgpurpose640@gmail.com>
2026-04-27 18:09:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "8635942785")) # Owner id
 PORT = os.environ.get("PORT", "8001")
 #--------------------------------------------
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://aarshhub:wcCgmKoCu2sTsEtv@cluster0.6shiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DATABASE_NAME", "Dark")
+DB_NAME = os.environ.get("DATABASE_NAME", "Darjk")
 #--------------------------------------------
 FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "10"))  # 0 means no expiry
 BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/Xsupprtxbot")
