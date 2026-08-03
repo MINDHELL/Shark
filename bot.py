d7ebb804b5c6f7aae4a96fa21c40a084c7d788ea
MINDHELL <Tgpurpose640@gmail.com>
2026-08-03 12:49:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -65,9 +65,7 @@ class Bot(Client):
             name="Bot",
             api_hash=API_HASH,
             api_id=APP_ID,
-            plugins={
-                "root": "plugins"
-            },
+            plugins= None,
             workers=TG_BOT_WORKERS,
             bot_token=TG_BOT_TOKEN
         )
